6f6c195a8bcdb43a07b78fe37966a447196613f1
Phillip Ball <pball@mail.greenriver.edu>
2021-04-27 13:47:17 -0400

    Added the controller

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// this is my controller for the diner project
+
+// turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require autoload file
+require_once('vendor/autoload.php');
+
+// :: invoke static method, -> invoke instance method
+// instantiate Fat-Free
+$f3 = Base::instance();
+
+// define routes
+// default route
+$f3->route('GET /', function (){
+    // display the home page
+    $view = new Template();
+    echo"Pets Home";
+    //echo $view->render('views/home.html');
+});
+
+// run Fat-Free
+$f3->run();
